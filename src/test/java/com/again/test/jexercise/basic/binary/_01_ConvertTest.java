96a5efd4c93707143b6e8ef54b457052473274aa
DogRex <827814990@qq.com>
2018-02-21 11:26:36 +0800

    【测验·基础篇·二进制】二进制基础转换能力，测试代码变更

--- src/test/java/com/again/test/jexercise/basic/binary/_01_ConvertTest.java
+++ src/test/java/com/again/test/jexercise/basic/binary/_01_ConvertTest.java
@@ -10,10 +10,20 @@ import static org.junit.Assert.*;
  */
 public class _01_ConvertTest extends _01_Convert {
     @Test
-    public void testConvertInt() throws Exception {
+    public void testConvertInt_2() throws Exception {
         Assert.assertEquals("1010", convertInt(10, 2));
+    }
+
+    @Test
+    public void testConvertInt_8() throws Exception {
         Assert.assertEquals("12", convertInt(10, 8));
+    }
+
+    @Test
+    public void testConvertInt_16() throws Exception {
         Assert.assertEquals("A", convertInt(10, 16));
     }
+
+
 
 }
