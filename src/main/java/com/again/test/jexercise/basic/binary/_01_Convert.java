ffee8263eee99c1406fc1f314664621f8843a0c5
DogRex <827814990@qq.com>
2018-03-02 22:15:47 +0800

    【测验·基础篇·二进制】二进制转换代码优化

--- src/main/java/com/again/test/jexercise/basic/binary/_01_Convert.java
+++ src/main/java/com/again/test/jexercise/basic/binary/_01_Convert.java
@@ -1,8 +1,14 @@
 package com.again.test.jexercise.basic.binary;
+
 /**
  * Created by dogrex on 2018/2/21.
  */
 public class _01_Convert {
+
+    /**
+     * 编码规范，常量的标准写法，改成全部大写，单词之间下划线分割
+     */
+    private static final char[] HEX_ARRAY = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     /**
      * 做整数（10进制）与其他进制的转化<p>
@@ -18,31 +24,11 @@ public class _01_Convert {
      * @return 对应进制的字符串序列
      */
     public static String convertInt(int n, int radix) {
-//        if(radix==2){
-///           x=Integer.toBinaryString(n).toString();
-//
-//        }else if(radix==8){
-//            x=Integer.toOctalString(n).toString();
-//        }else if(radix==16){
-//            x=Integer.toHexString(n).toString();
-//        }
-        char[] arr={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        String in="";
-        while(n != 0){
-            int f = n % radix;
-            in+=arr[f];
-            n=(n-f)/radix;
-            if(n==0 ){break;}else if(n==1){in+="1";break;}
+        String result = "";
+        while (n != 0) {
+            result = HEX_ARRAY[n % radix] + result;
+            n /= radix;
         }
-
-        char[] arr2=in.toCharArray();
-        int y=Math.round(arr2.length/2);
-        for(int j =0;j<y;j++){
-            char z = arr2[j];
-            arr2[j]=arr2[arr2.length-1-j];
-            arr2[arr2.length-1-j]=z;
-        }
-        String x=String.valueOf(arr2);
-        return x;
+        return result;
     }
 }
